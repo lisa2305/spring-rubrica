e2fb083ada1419a77ba32ada46a263e191113b7d
lisa2305 <lisa.giordani@marconirovereto.it>
2024-04-09 08:29:04 +0200

    first commit

---
+++ src/main/java/it/marconi/springrubrica/SpringRubricaApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.springrubrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRubricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRubricaApplication.class, args);
+	}
+
+}
